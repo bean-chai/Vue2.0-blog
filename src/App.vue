265fb81b11c9895d6127ad267eebf2bfc261cf3f
bbxx123 <2787922490@qq.com>
2022-08-03 17:20:27 +0800

    制作Home并添加看板娘

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<!--
+ * @Author: fengyuanyao fengyuanyao@fanyu.com 
+ * @Date: 2022-08-03 15:33:01
+ * @LastEditors: fengyuanyao fengyuanyao@fanyu.com
+ * @LastEditTime: 2022-08-03 17:02:46
+ * @FilePath: \blog-demo\src\App.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by error: git config user.name && git config user.email & please set dead value or install git, All Rights Reserved. 
+-->
+<template>
+  <div id="app">
+    <router-view/>
+    <!-- <router-view /> -->
+		<!-- <div class="star" style="width: 200px; height: 300px; background-color: red;"> -->
+
+		<!-- </div> -->
+  </div>
+</template>
+<script>
+	export default {
+		name: 'App',
+		created() {
+			window.L2Dwidget
+				//此处是对点击人物时触发事件的监听
+				.on('*', (name) => {
+
+				})
+				//对人物的属性配置
+				.init({
+					pluginRootPath: '../live2dw/', //人物文件夹的位置
+					pluginJsPath: 'lib/',
+					pluginModelPath: 'live2d-widget-model-koharu/assets/',
+					model: {
+						jsonPath: '../live2dw/live2d-widget-model-koharu/assets/koharu.model.json'
+					},
+					dialog: {
+						enable: true, //是否开启对话框
+						script: {
+							//每20s，显示一言（调用一言Api返回的句子）
+							'every idle 20s': '$hitokoto$',
+							//触摸到class='star'对象,将会展示的文字
+							'hover .star': '星星在天上而你在我心里 (*/ω＼*)',
+							//触摸到身体
+							'tap body': '害羞⁄(⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄',
+							//触摸到头部
+							'tap face': '~~'
+						}
+					},
+					display: { //人物的属性配置
+						position: 'left',
+						width: 250,
+						height: 700,
+						hOffset: 100, // 横向偏移
+						vOffset: -220 // 纵向偏移
+					}, 
+					mobile: {
+						show: true, //是否在移动端展示
+						scale: 0.5 //t透明度
+					}
+				});
+		}
+
+	}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+		margin: 0;
+		width: 100%;
+	}
+
+	html {
+		width: 100%;
+		height: 100%;
+	}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,78 @@
+<!--
+ * @Author: fengyuanyao fengyuanyao@fanyu.com 
+ * @Date: 2022-08-03 15:33:01
+ * @LastEditors: fengyuanyao fengyuanyao@fanyu.com
+ * @LastEditTime: 2022-08-03 16:35:06
+ * @FilePath: \blog-demo\src\views\HomeView.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by error: git config user.name && git config user.email & please set dead value or install git, All Rights Reserved. 
+-->
+<template>
+  <div class="home">
+   <header>
+    <div class="addressName">chaichai.top</div>    
+    <div class="routerBox">
+      <router-link to="/" class="routerLink">首页</router-link>
+      <router-link to="/about" class="routerLink">文章</router-link>
+      <a href="https://marrydream.top" class="routerLink">友链</a>
+    </div>
+   </header>
+   <nav class="welecomeText">welecome to chaichai.top</nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+.home {
+   background: url('@/assets/bgc.jpeg') no-repeat;
+   width: 100%;
+  height: 100vh;
+header {
+  display: flex;
+  justify-content:space-between;
+  height: 95px;
+  width: 100%;
+  .addressName{
+    font-size: 30px;
+    width: 250px;
+    height: 95px;
+    line-height: 95px;
+    font-weight: 600;
+    color: #fff;
+  }
+  a {
+    text-decoration: none;
+  }
+  .routerBox{
+    display: flex;
+    justify-content:space-between;
+    width: 200px;
+    margin-right: 30px;
+    .routerLink {
+    font-weight: 600;
+    color: #fff;
+    height: 95px;
+    line-height: 95px;
+   }
+  }
+ }
+ .welecomeText {
+  line-height: calc(100vh - 180px);
+  text-align: center;
+  font-weight: 600;
+  color: #fff;
+  font-size: 50px;
+ }
+}
+
+</style>
